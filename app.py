4965469f785da9e24c6f198c4c19b55c018b2941
adzik strymerenciusz <20097873@mail.wit.ie>
2025-08-07 11:51:40 +0100

    Added graphs for locations and companies

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import os
 from collections import Counter
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 
 st.title("Computing Job Keyword Analyser")
 
@@ -50,3 +51,32 @@ if st.button("Analyse"):
     plt.xticks(rotation=75)
     plt.tight_layout()
     st.pyplot(fig)
+
+    st.subheader("Top Job Locations")
+    df["Location"] = df["Location"].apply(
+    lambda x: "Dublin" if "dublin" in str(x).lower() else x.strip()
+    )
+    location_counts = df["Location"].value_counts().head(10)
+
+    fig1, ax1 = plt.subplots()
+    ax1.bar(location_counts.index, location_counts.values, color="skyblue")
+    ax1.set_xlabel("Location")
+    ax1.set_ylabel("Number of Jobs")
+    ax1.set_title("Job Locations")
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    st.pyplot(fig1)
+
+
+    st.subheader("Top Hiring Companies")
+    company_counts = df["Company"].value_counts().head(10)
+
+    fig2, ax2 = plt.subplots()
+    ax2.bar(company_counts.index, company_counts.values, color="salmon")
+    ax2.set_xlabel("Company")
+    ax2.set_ylabel("Number of Jobs")
+    ax2.set_title("Hiring Companies")
+    ax2.yaxis.set_major_locator(mticker.MaxNLocator(integer=True))
+    plt.xticks(rotation=90)
+    plt.tight_layout()
+    st.pyplot(fig2)
